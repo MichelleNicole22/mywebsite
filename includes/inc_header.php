0bbcea3b1f9e8faa2075930d60a5b450dc40a3e8
MichelleNicole22 <velvetstorm22@gmail.com>
2016-07-23 17:07:01 -0700

    Add header

---
+++ includes/inc_header.php
@@ -0,0 +1,13 @@
+<header>
+	<nav class="navbar navbar-fixed-top">
+		<div class="navbar-header">
+			<a href="#" class="navbar-brand">Michelle N. Rodriguez</a>
+		</div>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="http://michellenrodriguez.com/index.php">Home</a></li>
+				<li><a href="http://michellenrodriguez.com/about.php">About</a></li>
+				<li><a href="http://michellenrodriguez.com/portfolio.php">Portfolio</a></li>
+				<li><a href="http://michellenrodriguez.com/contact.php">Contact</a></li>
+			</ul>
+	</nav>
+</header>
